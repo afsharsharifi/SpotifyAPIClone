226c5c56e0197a4ca249c673de51b869fc5b355b
Afshar Sharifi <afsharsharifi2020@gmail.com>
2024-01-04 17:10:44 +0330

    User added for comments

--- home/api/serializers.py
+++ home/api/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
+from authentication.api.serializers import UserRegisterSerializer
+
 from ..models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    user = UserRegisterSerializer(read_only=True)
+
     class Meta:
         model = Review
         fields = ("id", "user", "comment", "created_at", "updated_at")
